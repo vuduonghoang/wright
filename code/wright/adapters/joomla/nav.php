0a3ba7689e071cd4d69ca3416776afed87de6803
Lu Nguyen <lu@redweb.dk>
2013-11-26 13:40:35 +0700

    Fix nav container

--- code/wright/adapters/joomla/nav.php
+++ code/wright/adapters/joomla/nav.php
@@ -22,19 +22,24 @@ class WrightAdapterJoomlaNav
 		if (!isset($args['type']))
 			$args['type'] = 'menu';
 
+		if (!isset($args['containerClass']))
+			$args['containerClass'] = '';
+
 		$nav = '<div class="' . $args['wrapper'] . '">
 					<nav id="' . $args['name'] . '" class="navbar ' . $args['wrapClass'] . '"  role="navigation">
-						<div class="navbar-header">
-						    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-' . $args['name'] . '">
-						      <span class="sr-only">Toggle navigation</span>
-						      <span class="icon-bar"></span>
-						      <span class="icon-bar"></span>
-						      <span class="icon-bar"></span>
-						    </button>
-					  	</div>
+						<div class="' . $args['containerClass'] . '">
+							<div class="navbar-header">
+							    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-' . $args['name'] . '">
+							      <span class="sr-only">Toggle navigation</span>
+							      <span class="icon-bar"></span>
+							      <span class="icon-bar"></span>
+							      <span class="icon-bar"></span>
+							    </button>
+						  	</div>
 
-	  					<div class="collapse navbar-collapse" id="nav-' . $args['name'] . '" >
-	  						<jdoc:include type="modules" name="' . $args['name'] . '" style="' . $args['style'] . '" />
+		  					<div class="collapse navbar-collapse" id="nav-' . $args['name'] . '" >
+		  						<jdoc:include type="modules" name="' . $args['name'] . '" style="' . $args['style'] . '" />
+		  					</div>
 	  					</div>
 					</nav>
 				</div>';
