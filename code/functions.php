f3271235e6cb2013c5c764cb4dbf3ffd02e55ab7
Lu Nguyen Nhat <lu@redweb.dk>
2013-12-25 14:36:27 +0700

    Clear up functions

--- code/functions.php
+++ code/functions.php
@@ -9,3 +9,19 @@
  
 // Restrict Access to within Joomla
 defined('_JEXEC') or die('Restricted access');
+
+// Get the bootstrap row mode ( row / row-fluid )
+$gridMode = $this->params->get('bs_rowmode', 'row-fluid');
+$containerClass = 'container';
+
+if ($gridMode == 'row-fluid')
+{
+	$containerClass = 'container-fluid';
+}
+
+$bodyclass = "";
+
+if ($this->countModules('toolbar'))
+{
+	$bodyclass = "toolbarpadding";
+}
--- code/template.php
+++ code/template.php
@@ -9,22 +9,6 @@
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
-// Get the bootstrap row mode ( row / row-fluid )
-$gridMode = $this->params->get('bs_rowmode', 'row-fluid');
-$containerClass = 'container';
-
-if ($gridMode == 'row-fluid')
-{
-	$containerClass = 'container-fluid';
-}
-
-$bodyclass = "";
-
-if ($this->countModules('toolbar'))
-{
-	$bodyclass = "toolbarpadding";
-}
-
 ?>
 <!DOCTYPE html>
 	<html>
